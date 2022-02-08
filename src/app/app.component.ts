1f15bd11c5ca4cc57a9a66b6168575aa73aa22c1
Volodymyr Chykharivskyi <v.chykharivskyi@tenantcloud.com>
2022-02-08 22:59:37 +0200

    Added post request for create new Todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,29 @@ import { Todo } from './interfaces/todo.interface';
 export class AppComponent implements OnInit {
 	public todos: Todo[] = [];
 
+	public todoTitle = '';
+
 	constructor(private http: HttpClient) {}
 
 	public ngOnInit() {
 		this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2').subscribe(resp => {
 			this.todos = resp;
+		});
+	}
+
+	public onAddTodo(): void {
+		if (!this.todoTitle.trim()) {
+			return;
+		}
+
+		const newTodo: Todo = {
+			title: this.todoTitle,
+			completed: false
+		}
+
+		this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo).subscribe(resp => {
+			this.todos.push(resp);
+			this.todoTitle = '';
 		});
 	}
 }
