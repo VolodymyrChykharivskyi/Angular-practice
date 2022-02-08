dfa083f8cd37f3c7ee86e3da3b47da229218584e
Volodymyr Chykharivskyi <v.chykharivskyi@tenantcloud.com>
2022-02-08 22:52:30 +0200

    Added get request for get data from jsonplaceholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Todo } from './interfaces/todo.interface';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-	title = 'angular-practice';
+export class AppComponent implements OnInit {
+	public todos: Todo[] = [];
+
+	constructor(private http: HttpClient) {}
+
+	public ngOnInit() {
+		this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2').subscribe(resp => {
+			this.todos = resp;
+		});
+	}
 }
