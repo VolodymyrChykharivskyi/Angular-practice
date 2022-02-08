7f072b04a5925ec3d7269596cc3eef510d190988
Volodymyr Chykharivskyi <v.chykharivskyi@tenantcloud.com>
2022-02-08 23:06:29 +0200

    Added base loader for todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { Todo } from './interfaces/todo.interface';
+import { delay } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-root',
@@ -11,14 +12,14 @@ import { Todo } from './interfaces/todo.interface';
 export class AppComponent implements OnInit {
 	public todos: Todo[] = [];
 
+	public isLoading = false;
+
 	public todoTitle = '';
 
 	constructor(private http: HttpClient) {}
 
 	public ngOnInit() {
-		this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2').subscribe(resp => {
-			this.todos = resp;
-		});
+		this.onFetchTotos();
 	}
 
 	public onAddTodo(): void {
@@ -28,12 +29,25 @@ export class AppComponent implements OnInit {
 
 		const newTodo: Todo = {
 			title: this.todoTitle,
-			completed: false
-		}
+			completed: false,
+		};
 
-		this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo).subscribe(resp => {
+		this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo).subscribe((resp) => {
 			this.todos.push(resp);
 			this.todoTitle = '';
 		});
+	}
+
+	public onFetchTotos(): void {
+		this.isLoading = true;
+
+		this.http
+			.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+			.pipe(delay(1500))
+			.subscribe((resp) => {
+				this.todos = resp;
+
+				this.isLoading = false;
+			});
 	}
 }
