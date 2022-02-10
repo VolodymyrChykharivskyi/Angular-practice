307cb465f5d76a8677637f992bbe15ae51d9967b
Volodymyr Chykharivskyi <v.chykharivskyi@tenantcloud.com>
2022-02-10 22:45:42 +0200

    Added interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { TodoRestService } from './services/toto-rest/todo-rest.service';
+import { AuthInterceptor } from './auth.interceptor';
+
+const INTERCEPTOR_PROVIDER: Provider = { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true };
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-	providers: [TodoRestService],
+	providers: [TodoRestService, INTERCEPTOR_PROVIDER],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/toto-rest/todo-rest.service.ts
+++ src/app/services/toto-rest/todo-rest.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Todo } from '../../interfaces/todo.interface';
 import { Observable, throwError } from 'rxjs';
-import { catchError, delay } from 'rxjs/operators';
+import { catchError, delay, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class TodoRestService {
@@ -18,20 +18,43 @@ export class TodoRestService {
 	}
 
 	public getItems(): Observable<Todo[]> {
-		return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos', { params: new HttpParams().set('_limit', '3') }).pipe(
-			delay(500),
-			catchError((error) => {
-				console.log('Error', error.message);
-				return throwError(error);
+		return this.http
+			.get<Todo[]>('https://jsonplaceholder.typicode.com/todos', {
+				params: new HttpParams().set('_limit', '3'),
+				observe: 'response',
+			})
+			.pipe(
+				map((response) => {
+					console.log(response);
+					return response.body;
+				}),
+				delay(500),
+				catchError((error) => {
+					console.log('Error', error.message);
+					return throwError(error);
+				})
+			);
+	}
+
+	public removeItem(id: number): Observable<any> {
+		return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`, { observe: 'events' }).pipe(
+			tap((event) => {
+				if (event.type === HttpEventType.Sent) {
+					console.log('Sent', event);
+				}
+
+				if (event.type === HttpEventType.Response) {
+					console.log('Response', event);
+				}
 			})
 		);
 	}
 
-	public removeItem(id: number): Observable<void> {
-		return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`);
-	}
-
 	public completeTodo(id: number): Observable<Todo> {
-		return this.http.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`, { completed: true });
+		return this.http.put<Todo>(
+			`https://jsonplaceholder.typicode.com/todos/${id}`,
+			{ completed: true },
+			{ responseType: 'json' }
+		);
 	}
 }
