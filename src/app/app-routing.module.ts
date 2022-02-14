22b2d58c69dc93257e8d64a5d2975021f878b08b
Volodymyr Chykharivskyi <v.chykharivskyi@tenantcloud.com>
2022-02-14 22:26:19 +0200

    Basic work with router and query params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'about', component: AboutComponent },
+	{ path: 'posts', component: PostsComponent },
+	{ path: 'posts/:id', component: PostComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import { AboutExtraComponent } from './components/about-extra/about-extra.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [AppComponent, AboutComponent, HomeComponent, PostsComponent, PostComponent, AboutExtraComponent],
+	imports: [BrowserModule, FormsModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
